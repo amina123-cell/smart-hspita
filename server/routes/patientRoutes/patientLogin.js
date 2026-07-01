9fbf1b3c6493ebc13947ecef4cea6ed25f14fe0e
z1z2a1a2123456789-source <z1z2a1a2123456789@gmail.com>
2026-07-01 10:42:34 +0200

    Upload project

--- server/routes/patientRoutes/patientLogin.js
+++ server/routes/patientRoutes/patientLogin.js
@@ -1,14 +1,6 @@
-// server/routes/patientRoutes/patientLogin.js
+ //server/routes/patientRoutes/patientLogin.js
 const connectDB = require("../../database/dbconnection");
-//const Patient = require("../../models/patientModel");
-
-const path = require('path');
-// ✅ صح: كيحسب المسار انطلاقاً من المجلد الحالي (__dirname)
-const Patient = require(path.join(__dirname, '../../models/PatientModel'));
-
-
-
-
+const Patient = require("../../models/patientModel");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 
